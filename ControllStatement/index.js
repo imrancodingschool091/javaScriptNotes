da34cde3e94082116ce1a93861b23a69594cde47
suman <imrancodingschool@gmail.com>
2024-12-07 13:03:42 +0530

    added Controll Statement Folder

---
+++ ControllStatement/index.js
@@ -0,0 +1,55 @@
+//for loop
+
+//print a table of any number taking input from user
+
+// let  input =prompt("Enter a number ..")
+
+// for(let i=1; i<=10; i++){
+
+//     console.log(`${input}*${i} = ${input*i}`);
+// }
+
+
+// for(let i=0; i<=100; i++){
+//     // console.log(i);
+//    if (i==12) {
+//     continue;
+    
+//    }
+//    console.log(i);
+
+// }
+
+//do while loop
+// let i = 0;
+
+// do {
+//   console.log(i);
+//   i++; // Increment i
+// } while (i <= 10); // Condition comes after the loop block
+
+
+// let i=0;
+
+// while (i<=10) {
+//     console.log(i);
+//     i++;
+    
+// }
+
+
+
+//guess my  favirate player
+
+// let input;
+
+// while (true) {
+//     input = prompt("Guess my favorite player");
+    
+//     if (input.toLowerCase() === "rohit") {  // Use .toLowerCase() to make it case-insensitive
+//         console.log("Congratulations! You have guessed successfully.");
+//         break;  // Exit the loop when the correct player is guessed
+//     } else {
+//         alert("try again !")
+//     }
+// }
