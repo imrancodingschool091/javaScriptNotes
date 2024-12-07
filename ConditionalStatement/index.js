ed2fc72426fa819f856d17dc247bd4aea4d8a6c3
suman <imrancodingschool@gmail.com>
2024-12-07 15:46:02 +0530

    added new folder

---
+++ ConditionalStatement/index.js
@@ -0,0 +1,83 @@
+//if 
+
+// let password=prompt("Enter Your Password");
+
+// if (password==786) {
+//     console.log("Unlockd Device!");
+    
+// }
+
+// else{
+//     alert("try again!")
+// }
+
+//else if
+
+// let marks=prompt("Enter Your Marks!");
+
+// if (marks >=60 && marks <=100) {
+//     alert("Congrates ! you got first Devision!")
+    
+// }
+// else if(marks <60 && marks >=45){
+//     alert("Congrates ! you got Second Devision!")
+
+// }
+
+
+// else if (marks >=33 && marks <45) {
+//     alert("Congrates ! you got Third Devision!")
+    
+// }
+
+// else if (marks >0 && marks <33) {
+//     alert("Unfortunatily ! you are fail!")
+    
+// }
+
+// else {
+//     alert("bad expression! try again")
+// }
+
+
+//find the largest number using else if
+
+// let a=10;
+// let b=7;
+// let c=44;
+
+// if (a>b && a>c) {
+//     alert("a is the larget number")
+    
+// }
+
+// else if(b>a && b>c){
+//     alert("b is the larget number")
+
+// }
+
+// else {
+//     alert("c is the largest number")
+// }
+
+
+//nested if
+
+// let age=prompt("Enter your Age");
+
+// if (age>18) {
+
+//     if (age<70) {
+//         alert("you can vote")
+        
+//     }
+//     else{
+//         alert("you are too old to vote")
+//     }
+    
+// }
+
+
+// let age=1
+
+//  console.log(age>=18 ? "you can vote":"you can not vote");
